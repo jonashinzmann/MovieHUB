30786c48fb7a05ac0d1c0159e7fb914a4a4683f9
Jonas Hinzmann <jonas.hinzmann@htp-tel.de>
2023-07-26 10:54:10 +0200

    change fetch

--- pages/api/movies.js
+++ pages/api/movies.js
@@ -1,6 +1,6 @@
 
 export default async function handler(req, res) {
-    const API_KEY = process.env.NEXT_PUBLIC_MOVIE_HUB_API_KEY;
+    const API_KEY = process.env.MOVIE_HUB_API_KEY;
     const response = await fetch(`http://www.omdbapi.com/?s=avengers&apikey=${API_KEY}`);
     const data = await response.json();
     res.status(200).json(data);
