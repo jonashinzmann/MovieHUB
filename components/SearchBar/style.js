cdb040466fd29d6ac22ed369207df180bc4a26b8
Jonas Hinzmann <jonas.hinzmann@htp-tel.de>
2023-07-31 10:15:48 +0200

    add SearchBar

---
+++ components/SearchBar/style.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const StyledInput = styled.input`
+  margin-bottom: 0.625rem;
+  padding: 0.313rem;
+  border-radius: 0.313rem;
+  width: 15.625rem;
+  height: 2.188rem;
+`;
+
+export const StyledButton = styled.button`
+  background-color: #7693a6;
+  color: white;
+  padding: 0.625rem 1.25rem;
+  border: none;
+  border-radius: 0.313rem;
+  cursor: pointer;
+  width: 8rem;
+  font-size: 1rem;
+`;
